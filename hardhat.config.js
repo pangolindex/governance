d5221090632aac8aaf402b0162f0b579dd213bb8
Brandon <brandon@pangolin.exchange>
2021-09-07 15:01:48 -0400

    Easy fork

--- hardhat.config.js
+++ hardhat.config.js
@@ -67,29 +67,22 @@ module.exports = {
   },
   networks: {
     hardhat: {
-      gasPrice: 470000000000,
-      chainId: 43112,
-      initialDate: "2021-01-01",
-    },
-    avash: {
-      url: 'http://localhost:9650/ext/bc/C/rpc',
-      gasPrice: 470000000000,
-      chainId: 43112,
-      accounts: ["0x56289e99c94b6912bfc12adc093c9b51124f0dc54ac7a766b2bc5ccf558d8027"]
+      gasPrice: 225000000000,
+      chainId: 43114
     },
     local: {
       url: 'http://127.0.0.1:8545',
       gasPrice: 225000000000,
-      chainId: 43112,
+      chainId: 43114,
+      // accounts: [""]
     },
     fuji: {
       url: 'https://api.avax-test.network/ext/bc/C/rpc',
-      gasPrice: 225000000000,
-      chainId: 43113,
+      gasPrice: 225000000000
     },
     mainnet: {
       url: 'https://api.avax.network/ext/bc/C/rpc',
-      gasPrice: 225000000000,
+      gasPrice: 225000000000
     }
   }
 };
